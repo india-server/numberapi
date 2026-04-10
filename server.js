dfec21e3cf598682b6db51abfee69d30fa1450fe
india-server <german.humble@gmail.com>
2026-04-11 04:10:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -458,7 +458,7 @@ app.get("/lookup", async (req, res) => {
   const { error, status, keyDoc } = await validateApiKey(apiKey, "number");
   if (error) return res.status(status).json({ error });
   try {
-    const response = await axios.get(process.env.UPSTREAM_API_URL + "?type=mobile&term=" + encodeURIComponent(number), { timeout: 10000 });
+    const response = await axios.get(process.env.UPSTREAM_API_URL + "?key=${process.env.LOOKUP_API_KEY}&type=number&number=" + encodeURIComponent(number), { timeout: 10000 });
     await incrementUsage(keyDoc._id);
     const data = response.data;
     data.owner = "@aerivue";
